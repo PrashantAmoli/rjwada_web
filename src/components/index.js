94911ae7f69a2da8a4cf94af1bb6d6d853910b28
Prashant <prashantamoli@outlook.com>
2023-01-25 00:37:51 +0530

    ++pages with routing

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+// * /pages components
+/*
+All components will be used in the pages folder components
+use cards and layout components within pages components
+this is the central export destination for all the components with a route
+*/
+import Home from './pages/Home';
+import About from './pages/About';
+import Careers from './pages/Careers';
+import Shopify from './pages/Shopify';
+import Features from './pages/Features';
+import Bigcommerce from './pages/Bigcommerce';
+import ScheduleDemo from './pages/ScheduleDemo';
+import Stepin from './pages/Stepin';
+import Services from './pages/Services';
+import Blogs from './pages/Blogs';
+
+export { Home, About, Shopify, Features, Bigcommerce, ScheduleDemo, Stepin, Services, Blogs, Careers };
