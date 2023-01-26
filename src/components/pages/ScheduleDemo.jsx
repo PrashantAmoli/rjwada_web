755199e6974c4be5c852aa5f5b74128a36f2fb4e
Prashant <prashantamoli@outlook.com>
2023-01-26 20:15:30 +0530

    ++stepin UI

--- src/components/pages/ScheduleDemo.jsx
+++ src/components/pages/ScheduleDemo.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import ScheduleForm from '../cards/ScheduleForm';
+import Container from 'react-bootstrap/Container';
 
 function ScheduleDemo() {
-	return <div>ScheduleDemo</div>;
+	return (
+		<section className="bgMain">
+			<Container fluid="xl" className="justify-content-md-center">
+				<ScheduleForm />
+			</Container>
+		</section>
+	);
 }
 
 export default ScheduleDemo;
