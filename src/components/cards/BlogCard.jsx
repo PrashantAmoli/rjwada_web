12cc4b8a8333f3ca03b3a42501d8f1df10d3cf28
Prashant <prashantamoli@outlook.com>
2023-01-26 20:15:53 +0530

    ++blogs UI

--- src/components/cards/BlogCard.jsx
+++ src/components/cards/BlogCard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from '../../styles/Blogs.module.css';
 
-export default function BlogCard() {
-  return (
-    <div>BlogCard</div>
-  )
+const temp = {
+	category: 'Product Update',
+	title: 'Augmented Reality (AR) is an interactive experience of a real-world environment where the objects',
+	link: 'https://PrashantAmoli.github.io',
+	thumbnail: '/blog-thumbnail.svg',
+};
+
+export default function BlogCard({ thumbnail = temp.thumbnail, category = temp.category, title = temp.title, link = temp.link }) {
+	return (
+		<div className={`${styles.blogCard}`}>
+			<img src={thumbnail} alt="blog" className={`${styles.blogThumbnail}`} />
+
+			<div className="flex flex-col">
+				<h3 className="">Blog Title</h3>
+
+				<p>Augmented reality (AR) is an interactive experience of a real-world environment where the objects</p>
+
+				<Link to={link} className={``}>
+					<img src="/blog-arrow.svg" alt="" />
+					&nbsp;READ FULL ARTICLE
+				</Link>
+			</div>
+		</div>
+	);
 }
--- src/components/pages/Blogs.jsx
+++ src/components/pages/Blogs.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import { Button } from 'react-bootstrap';
+import styles from '../../styles/Blogs.module.css';
+import BlogCard from '../cards/BlogCard';
+// Bootstrap
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+
+const topics = ['All', 'Product Update', 'Augmented Reality', 'Technology', 'Expert Opinion'];
 
 function Blogs() {
-	return <div>Blogs</div>;
+	return (
+		<Container fluid className={`${styles.pageContainer}`}>
+			<header className={`${styles.header}`}>
+				<h1 className="">BLOG</h1>
+
+				<div className={`${styles.tabbar}`}>
+					{topics.map((topic, index) => (
+						<button key={index} className={`${styles.topicBtn} ${index === 0 ? styles.active : ''}`}>
+							{topic}
+						</button>
+					))}
+				</div>
+			</header>
+
+			<div className={`${styles.mainArticle}`}>
+				<img src="/blog-article.svg" alt="" />
+
+				<div className={`${styles.mainArticleBody}`}>
+					<h2>Why AR clothing try-on is nearly here?</h2>
+
+					<p>The ability to try on clothes digitally has been a long-term goal that tech companies of all sizes are close to reaching. Here's why.</p>
+
+					<Link to={'/scheduleDemo'} className={``}>
+						<img src="/blog-arrow.svg" alt="" />
+						&nbsp;READ FULL ARTICLE
+					</Link>
+				</div>
+			</div>
+
+			<div className={`${styles.blogsGrid}`}>
+				<BlogCard />
+				<BlogCard />
+				<BlogCard />
+				<BlogCard />
+				<BlogCard />
+				<BlogCard />
+				<BlogCard />
+			</div>
+		</Container>
+	);
 }
 
 export default Blogs;
