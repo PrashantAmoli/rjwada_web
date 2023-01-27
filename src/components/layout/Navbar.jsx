aa8079fa9eb7a64cd88f227275c77cb006baad54
apoorvsingh2314 <apoorvsingh2314@gmail.com>
2023-01-28 00:21:45 +0530

    update navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,58 +1,34 @@
 import React from 'react'
 import '../layout/Navbar.css'
-import { Link } from 'react-router-dom'
-
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function navbar() {
   return (
 
-    // <Navbar id="navbar" collapseOnSelect expand="md" >
-    //   <Container>
-    //     <Navbar.Brand href="#brand" id="nav-list">rjwada</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="responsive-navbar-nav" id="navbar-toggler"/>
-    //     <Navbar.Collapse id="responsive-navbar-nav">
-    //       <Nav style={{ marginLeft:'auto'}} >
+    <Navbar id="navbar" collapseOnSelect expand="md" >
+      <Container>
+        <Navbar.Brand href="#brand" id="nav-list">rjwada</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" id="navbar-toggler"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav style={{ marginLeft:'auto'}} >
 
-    //         <Nav.Link href="./Features" id="nav-list">Features</Nav.Link>
+            <Nav.Link href="./Features" id="nav-list">Features</Nav.Link>
 
-    //         <NavDropdown title="Integration" id="nav-list" menuVariant="dark" align="end">
-    //           <NavDropdown.Item href="./Shopify" id="shopify">Shopify</NavDropdown.Item>
-    //         </NavDropdown>
+            <NavDropdown title="Integration" id="nav-list" menuVariant="dark" align="end">
+              <NavDropdown.Item href="./Shopify" id="shopify">Shopify</NavDropdown.Item>
+            </NavDropdown>
 
-    //         <Nav.Link href="./Careers" id="nav-list">Careers</Nav.Link>
+            <Nav.Link href="./Careers" id="nav-list">Careers</Nav.Link>
 
-    //         <Nav.Link href="./About" id="nav-list">About Us</Nav.Link>
+            <Nav.Link href="./About" id="nav-list">About Us</Nav.Link>
 
-    //         <Nav.Link href="./ScheduleDemo" id="nav-schedule-demo">Schedule a demo</Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
-
-    <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <Link to="/careers">Careers</Link>
-        </li>
-      </ul>
-    </nav>
-    </>
+            <Nav.Link href="./ScheduleDemo" id="nav-schedule-demo">Schedule a demo</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
